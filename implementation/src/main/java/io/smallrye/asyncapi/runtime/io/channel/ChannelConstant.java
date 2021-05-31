72f85e9df6a4429a90b5f8debe2da7157a720ddb
GasperV <gasper.vrhovsek@global.id>
2021-05-31 09:47:46 +0200

    Add dotname constants for channel

--- implementation/src/main/java/io/smallrye/asyncapi/runtime/io/channel/ChannelConstant.java
+++ implementation/src/main/java/io/smallrye/asyncapi/runtime/io/channel/ChannelConstant.java
@@ -1,7 +1,13 @@
 package io.smallrye.asyncapi.runtime.io.channel;
 
+import io.smallrye.asyncapi.spec.annotations.channels.ChannelItem;
+import io.smallrye.asyncapi.spec.annotations.channels.ChannelItems;
+import org.jboss.jandex.DotName;
+
 public class ChannelConstant {
 
+    public static final DotName DOTNAME_CHANNEL = DotName.createSimple(ChannelItem.class.getName());
+    public static final DotName DOTNAME_CHANNELS = DotName.createSimple(ChannelItems.class.getName());
     public static final String PROP_NAME = "name";
     public static final String PROP_DESCRIPTION = "description";
 }
